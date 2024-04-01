912be17b1f419440eb49ee69ca54a3afaa025ac4
MathieuVDB28 <mathios2000@gmail.com>
2024-04-01 21:48:50 +0200

    add .env variable / add convertion with api

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
 import "./globals.css";
+import Header from '@/app/components/Header';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html>
+    <body className={inter.className}>
+      <main className="flex min-h-screen flex-col items-center justify-center p-24">
+        <Header/>
+        {children}
+      </main>
+    </body>
     </html>
-  );
+);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,68 +1,117 @@
-import Image from "next/image";
+'use client';
+
+import {useState, useEffect} from "react";
+
+interface Currency {
+    code: string;
+    name: string;
+}
+
+interface Currency {
+    code: string;
+    name: string;
+}
 
 export default function Home() {
-  return (
-      <main className="flex min-h-screen flex-col items-center justify-center p-24">
-        {/* Section en haut de la page */}
-        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-            Get started by editing&nbsp;
-            <code className="font-mono font-bold">app/page.tsx</code>
-          </p>
-          <div
-              className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none"
-          >
-            <a
-                className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-                href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                  src="/vercel.svg"
-                  alt="Vercel Logo"
-                  className="dark:invert"
-                  width={100}
-                  height={24}
-                  priority
-              />
-            </a>
-          </div>
-        </div>
+    const [currencies, setCurrencies] = useState<Currency[]>([]);
+    const [fromCurrency, setFromCurrency] = useState<string>("");
+    const [toCurrency, setToCurrency] = useState<string>("");
+    const [amount, setAmount] = useState<number | undefined>(undefined);
+    const [conversionRate, setConversionRate] = useState<number | undefined>(undefined);
+    const [convertedAmount, setConvertedAmount] = useState<number | undefined>(undefined);
 
+    useEffect(() => {
+      const apiURL = `https://api.currencyapi.com/v3/currencies?apikey=${process.env.NEXT_PUBLIC_API_KEY}`;
+        fetch(apiURL)
+            .then((response) => response.json())
+            .then((data) => {
+                const currencyData = data?.data;
+                if (currencyData) {
+                    const currencyArray = Object.keys(currencyData).map((key) => ({
+                        code: key,
+                        name: currencyData[key].name,
+                    }));
+                    setCurrencies(currencyArray);
+                }
+            })
+            .catch((error) => console.error("Erreur lors de la récupération des devises:", error));
+    }, []);
+
+    const handleConvert = () => {
+        if (fromCurrency && toCurrency && amount) {
+            const apiUrl = `https://api.currencyapi.com/v3/latest?base_currency=${fromCurrency}&currencies[]=${toCurrency}&apikey=${process.env.NEXT_PUBLIC_API_KEY}`;
+            fetch(apiUrl)
+                .then((response) => response.json())
+                .then((data) => {
+                    const rate = data?.data?.[toCurrency]?.value;
+                    setConversionRate(rate);
+                    if (rate) {
+                        const converted = amount * rate;
+                        setConvertedAmount(converted);
+                    }
+                })
+                .catch((error) => console.error("Erreur lors de la récupération du taux de change:", error));
+        }
+    };
+
+    return (
         <div className="text-center flex-grow mt-40">
-          <div className="flex flex-col justify-center">
-            <h2>Convertisseur de devises</h2>
-            <div className="flex flex-col items-center justify-center">
-              <div className="mb-4">
-                <label htmlFor="fromCurrency">De :</label>
-                <select
-                    id="fromCurrency"
-                    className="border border-gray-300 rounded-md px-2 py-1 mx-2"
-                >
-                  <option value="USD">USD</option>
-                  <option value="EUR">EUR</option>
-                  <option value="GBP">GBP</option>
-                </select>
-              </div>
-              <div className="mb-4">
-                <label htmlFor="toCurrency">À :</label>
-                <select
-                    id="toCurrency"
-                    className="border border-gray-300 rounded-md px-2 py-1 mx-2"
-                >
-                  <option value="USD">USD</option>
-                  <option value="EUR">EUR</option>
-                  <option value="GBP">GBP</option>
-                </select>
-              </div>
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Convertir
-              </button>
+            <div className="flex flex-col justify-center">
+                <h2>Convertisseur de devises</h2>
+                <div className="flex flex-col items-center justify-center">
+                    <div className="mb-4">
+                        <label htmlFor="fromCurrency">De :</label>
+                        <select
+                            id="fromCurrency"
+                            value={fromCurrency}
+                            onChange={(e) => setFromCurrency(e.target.value)}
+                            className="text-[#020617] border border-gray-300 rounded-md px-2 py-1 mx-2"
+                        >
+                            <option value="">Sélectionner une devise</option>
+                            {currencies.map((currency) => (
+                                <option key={currency.code} value={currency.code}>
+                                    {currency.name}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="toCurrency">À :</label>
+                        <select
+                            id="toCurrency"
+                            value={toCurrency}
+                            onChange={(e) => setToCurrency(e.target.value)}
+                            className="text-[#020617] border border-gray-300 rounded-md px-2 py-1 mx-2"
+                        >
+                            <option value="">Sélectionner une devise</option>
+                            {currencies.map((currency) => (
+                                <option key={currency.code} value={currency.code}>
+                                    {currency.name}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="amount">Montant à convertir :</label>
+                        <input
+                            id="amount"
+                            type="number"
+                            value={amount}
+                            onChange={(e) => setAmount(parseFloat(e.target.value))}
+                            className="text-[#020617] border border-gray-300 rounded-md px-2 py-1 mx-2"
+                        />
+                    </div>
+                    <button onClick={handleConvert}
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Convertir
+                    </button>
+                    {conversionRate && (
+                        <div className="mt-4">
+                            <p>Résultat de la conversion : {convertedAmount}</p>
+                        </div>
+                    )}
+                </div>
             </div>
-          </div>
         </div>
-      </main>
-  );
+    );
 }
